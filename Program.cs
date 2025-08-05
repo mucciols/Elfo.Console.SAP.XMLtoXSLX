0df6761dec74b181ccab54e4ea7da81f1be6bb0e
Simone Muccioli <simone.muccioli@elfo.net>
2025-08-05 11:45:00 +0200

    aggiornamento

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Elfo.Console.SAP.XMLtoSSLX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Excel.Application();
+
+            string[] files = Directory.GetFiles("D:\\Projects\\Elfo.Console.SAP.CSVtoXML\\bin\\Debug\\net8.0\\OUT\\");
+
+            string directoryOut = "D:\\Projects\\Elfo.Console.SAP.CSVtoXML\\bin\\Debug\\net8.0\\OUT\\XSLX\\";
+
+            Directory.CreateDirectory(directoryOut);
+
+            foreach (string file in files)
+            {
+                var wb1 = app.Workbooks.OpenXML(file);
+
+                string fileNameOutput = directoryOut + Path.GetFileNameWithoutExtension(file) + ".xlsx";
+
+                wb1.SaveAs(fileNameOutput, Excel.XlFileFormat.xlOpenXMLWorkbook);
+
+                wb1.Close();
+            }
+            app.Quit();
+        }
+    }
+}
